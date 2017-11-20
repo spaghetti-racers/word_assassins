88c1f61953751809b947118581861b6f4c5fa0d9
Eric Garnett <egcodes@gmail.com>
2017-11-20 12:22:26 -0500

    access word array from db

--- app/components/Board.jsx
+++ app/components/Board.jsx
@@ -7,23 +7,42 @@ export default class Board extends Component {
     super(props)
     this.state = {
       // cards: [ {card0: {word: 'big'}}, {card1: {word: 'forest'}}, {card2: {word: 'ogre'}}, {card3: {word: 'stomach'}}, {card4: {word: 'tokyo'}}, {card5: {word: 'antler'}} ]
-      cards: []
+      cards: [],
+      words: []
     }
   }
 
+  // componentDidMount() {
+  //   const cards = firebase.database().ref('cards/')
+  //   cards.on('value', snapshot => {
+  //     const cardsFromDB = snapshot.val()
+  //     const cardArray = []
+  //     const cardKeys = Object.keys(snapshot.val())
+  //     for (let i=0; i < cardKeys.length; i++) {
+  //       cardArray.push(cardsFromDB[cardKeys[i]])
+  //     }
+  //     console.log('!!!!!!!!!!!!!! The pushed to final array', cardArray)
+  //     console.log('~~~~~~~~~~~~ The keys from the snapshot', cardKeys)
+  //     console.log('============ The snapshot from the firebase DB', snapshot.val())
+  //     this.setState({cards: cardArray})
+  //   })
+  // }
+
   componentDidMount() {
-    const cards = firebase.database().ref('cards/')
-    cards.on('value', snapshot => {
-      const cardsFromDB = snapshot.val()
-      const cardArray = []
-      const cardKeys = Object.keys(snapshot.val())
-      for (let i=0; i < cardKeys.length; i++) {
-        cardArray.push(cardsFromDB[cardKeys[i]])
-      }
-      console.log('!!!!!!!!!!!!!! The pushed to final array', cardArray)
-      console.log('~~~~~~~~~~~~ The keys from the snapshot', cardKeys)
-      console.log('============ The snapshot from the firebase DB', snapshot.val())
-      this.setState({cards: cardArray})
+    const words = firebase.database().ref('words/')
+    words.on('value', snapshot => {
+      //const wordsFromDB = snapshot.val()
+      const wordArray = snapshot.val()
+      console.log('!!!!!!!!!!!!! Is this an array?', wordArray)
+      //const cardKeys = Object.keys(snapshot.val())
+      // for (let i=0; i < cardKeys.length; i++) {
+      //   cardArray.push(cardsFromDB[cardKeys[i]])
+      // }
+      // console.log('!!!!!!!!!!!!!! The pushed to final array', cardArray)
+      // console.log('~~~~~~~~~~~~ The keys from the snapshot', cardKeys)
+      // console.log('============ The snapshot from the firebase DB', snapshot.val())
+      this.setState({words: wordArray})
+      console.log('~~~~~~~~~ Whats in state?', this.state.words)
     })
   }
 
