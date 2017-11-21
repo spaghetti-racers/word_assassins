76ade90b48d48198c0a3d5963a13afb3949c523c
Carmen Peralta <carmenilse@IPs-MacBook-Pro.local>
2017-11-21 16:46:09 -0500

    feat(check_hint): button is disable until user inputs a word and number

--- app/components/SpymasterHint.jsx
+++ app/components/SpymasterHint.jsx
@@ -5,12 +5,55 @@ export default class SpymasterHint extends Component {
   constructor() {
     super()
     this.state = {
-      display: false
+      numberOfWordsToGuess: '',
+      possibleHint: ''
     }
+    this.handlePossibleHint = this.handlePossibleHint.bind(this)
+    this.handleNumberChange = this.handleNumberChange.bind(this)
+    this.submitPotentialHint = this.submitPotentialHint.bind(this)
+  }
+  handlePossibleHint(event, data) {
+    this.setState({
+      possibleHint: event.target.value
+    }, () => {
+      console.log(this.state.possibleHint)
+    })
+  }
+  handleNumberChange(event) {
+    this.setState({
+      numberOfWordsToGuess: +event.target.value
+    }, () => {
+      console.log('this is the state ', this.state)
+    })
+  }
+  submitPotentialHint() {
+    const dataRef = firebase.database().ref()
+    dataRef.child('potentialHintandNumOfWords').set({
+      numberOfWordsToGuess: this.state.numberOfWordsToGuess,
+      possibleHint: this.state.possibleHint,
+    })
+    this.setState({
+      numberOfWordsToGuess: '',
+      possibleHint: ''
+    })
   }
   render() {
-    <div>
-      <h1>Display Spymaster hint </h1>
-    </div>
+    return (
+      <div>
+        <h1>Display Spymaster hint </h1>
+        <div className="ui input">
+          <input value={this.state.numberOfWordsToGuess} onChange={this.handleNumberChange} type="number" placeholder="enter a number" required/>
+          <input value={this.state.possibleHint} onChange={this.handlePossibleHint} type="text" placeholder="enter a hint"/>
+          <button
+            disabled={this.state.possibleHint.length<=2 ||this.state.numberOfWordsToGuess<1}
+            onClick = {this.submitPotentialHint}
+            className="ui button"
+          >
+          Confirm hint
+          </button>
+
+        </div>
+      </div>
+    )
   }
 }
