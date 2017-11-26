2d7b4a4534b38f0beaa6256ad3a313ccd4866639
Carmen <cperalta26@gmail.com>
2017-11-26 18:45:25 -0500

    clean active spymaster hinter  presenseter

--- app/components/presentational/ActiveSpymasterHinter.jsx
+++ app/components/presentational/ActiveSpymasterHinter.jsx
@@ -1,20 +1,38 @@
 import React from 'react'
+import { Button } from 'semantic-ui-react'
 
 const ActiveSpymasterHinter = props =>
 (
   <div>
     <h1>Display Spymaster hint </h1>
   <div className="ui input">
-      <input value={props.numberOfWordsToGuess} onChange={props.handleNumberChange} type="number" placeholder="enter a number" required disabled={props.hintApproval? 'disabled': null}/>
-      <input value={props.possibleHint} onChange={props.handlePossibleHint} type="text" placeholder="enter a hint" disabled={props.hintApproval? 'disabled': null} />
-      <button
+      <input
+        value={props.numberOfWordsToGuess}
+        onChange={props.handleNumberChange}
+        type="number" placeholder="enter a number"
+        disabled={props.hintApproval? 'disabled': null}
+      />
+
+      <input
+        value={props.possibleHint}
+        onChange={props.handlePossibleHint}
+        type="text" placeholder="enter a hint"
+        disabled={props.hintApproval? 'disabled': null}
+      />
+
+      <Button
         disabled={props.possibleHint.length<=2 || props.numberOfWordsToGuess<1}
         onClick = {props.submitPotentialHint}
-        className="ui button"
       >
-      Confirm hint
-      </button>
-      <button onClick = {props.resetHintGenerator} disabled={!props.hintApproval? 'disabled' : null}>Submit Hint</button>
+        Confirm hint
+      </Button>
+
+      <Button
+        disabled={!props.hintApproval? true : null}
+        onClick = {props.resetHintGenerator}
+      >
+        Submit Hint
+      </Button>
     </div>
   </div>
 )
