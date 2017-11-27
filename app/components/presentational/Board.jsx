2720b0854cc6771fb6e226d8000bece447c9c373
Eric Garnett <egcodes@gmail.com>
2017-11-27 14:48:08 -0500

    feat(spymaster): clean up comments

--- app/components/presentational/Board.jsx
+++ app/components/presentational/Board.jsx
@@ -1,26 +1,6 @@
 import React from 'react'
 import { Card } from 'semantic-ui-react'
 
-// const Board = props =>
-  // (
-  //   <div>
-  //     <Card.Group itemsPerRow={5}>
-  //       {
-  //         props.cards && props.cards.map((card, idx) =>
-  //           (
-  //             // LOGIC TO CHANGE COLOR AND WORD VIEW OF THE CARD UPON CLICK BASED ON COLOR/CLICKED ATTRIBUTES IN DB
-  //             <Card onClick={props.pickCard} style={{backgroundColor: card.clicked ? card.color : 'white'}} key={card.word}>
-  //               <Card.Content value={idx}>
-  //                 {
-  //                   card.clicked ? ' ' : <Card.Header value={idx}> {card.word} </Card.Header>
-  //                 }
-  //               </Card.Content>
-  //             </Card>)
-  //         )
-  //       }
-  //     </Card.Group>
-  //   </div>
-  // )
 const Board = props => (
   <div>
   {
