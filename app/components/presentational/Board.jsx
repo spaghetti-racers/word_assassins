d721c8cdedfa4e8f70e08813bacea08556007768
Rohan Saigal <rohan4saigal@gmail.com>
2017-11-28 15:46:22 -0500

    feat(board): display pass turn button under board

--- app/components/presentational/Board.jsx
+++ app/components/presentational/Board.jsx
@@ -1,64 +1,67 @@
 import React from 'react'
-import { Card } from 'semantic-ui-react'
+import { Card, Button } from 'semantic-ui-react'
 
 const Board = props => (
   <div>
-  {
-    props.players.player3.role === 'spymaster' ?
-    <div>
-      <Card.Group itemsPerRow={5}>
-        {
-          props.cards && props.cards.map((card, idx) =>
-            (
-              // LOGIC TO CHANGE COLOR AND WORD VIEW OF THE CARD UPON CLICK BASED ON COLOR/CLICKED ATTRIBUTES IN DB
-              <Card style={{ backgroundColor: card.color }} key={card.word}>
-                <Card.Content value={idx}>
-                  {
-                    <Card.Header value={idx}> {card.word} </Card.Header>
-                  }
-                </Card.Content>
-              </Card>)
-          )
-        }
-      </Card.Group>
+    {
+      props.players.player3.role === 'spymaster' ?
+        <div>
+          <Card.Group itemsPerRow={5}>
+            {
+              props.cards && props.cards.map((card, idx) =>
+                (
+                  // LOGIC TO CHANGE COLOR AND WORD VIEW OF THE CARD UPON CLICK BASED ON COLOR/CLICKED ATTRIBUTES IN DB
+                  <Card style={{ backgroundColor: card.color }} key={card.word}>
+                    <Card.Content value={idx}>
+                      {
+                        <Card.Header value={idx}> {card.word} </Card.Header>
+                      }
+                    </Card.Content>
+                  </Card>)
+              )
+            }
+          </Card.Group>
+        </div>
+        : props.players.player3.teamColor === props.currentGameStatus.activeTeam ?
+          <div>
+            <Card.Group itemsPerRow={5}>
+              {
+                props.cards && props.cards.map((card, idx) =>
+                  (
+                    // LOGIC TO CHANGE COLOR AND WORD VIEW OF THE CARD UPON CLICK BASED ON COLOR/CLICKED ATTRIBUTES IN DB
+                    <Card onClick={card.clicked ? null : props.pickCard} style={{ backgroundColor: card.clicked ? card.color : 'white' }} key={card.word}>
+                      <Card.Content value={idx}>
+                        {
+                          card.clicked ? ' ' : <Card.Header value={idx}> {card.word} </Card.Header>
+                        }
+                      </Card.Content>
+                    </Card>)
+                )
+              }
+            </Card.Group>
+          </div>
+          :
+          <div>
+            <Card.Group itemsPerRow={5}>
+              {
+                props.cards && props.cards.map((card, idx) =>
+                  (
+                    // LOGIC TO CHANGE COLOR AND WORD VIEW OF THE CARD UPON CLICK BASED ON COLOR/CLICKED ATTRIBUTES IN DB
+                    <Card style={{ backgroundColor: card.clicked ? card.color : 'white' }} key={card.word}>
+                      <Card.Content value={idx}>
+                        {
+                          card.clicked ? ' ' : <Card.Header value={idx}> {card.word} </Card.Header>
+                        }
+                      </Card.Content>
+                    </Card>)
+                )
+              }
+            </Card.Group>
+          </div>
+    }
+    <div className="passButtonWrapper">
+      <Button> PASS TURN</Button>
     </div>
-    : props.players.player3.teamColor === props.currentGameStatus.activeTeam ?
-    <div>
-      <Card.Group itemsPerRow={5}>
-        {
-          props.cards && props.cards.map((card, idx) =>
-            (
-              // LOGIC TO CHANGE COLOR AND WORD VIEW OF THE CARD UPON CLICK BASED ON COLOR/CLICKED ATTRIBUTES IN DB
-              <Card onClick={card.clicked ? null : props.pickCard} style={{backgroundColor: card.clicked ? card.color : 'white'}} key={card.word}>
-                <Card.Content value={idx}>
-                  {
-                    card.clicked ? ' ' : <Card.Header value={idx}> {card.word} </Card.Header>
-                  }
-                </Card.Content>
-              </Card>)
-          )
-        }
-      </Card.Group>
-    </div>
-    :
-    <div>
-      <Card.Group itemsPerRow={5}>
-        {
-          props.cards && props.cards.map((card, idx) =>
-            (
-              // LOGIC TO CHANGE COLOR AND WORD VIEW OF THE CARD UPON CLICK BASED ON COLOR/CLICKED ATTRIBUTES IN DB
-              <Card style={{backgroundColor: card.clicked ? card.color : 'white'}} key={card.word}>
-                <Card.Content value={idx}>
-                  {
-                    card.clicked ? ' ' : <Card.Header value={idx}> {card.word} </Card.Header>
-                  }
-                </Card.Content>
-              </Card>)
-          )
-        }
-      </Card.Group>
-    </div>
-  }
   </div>
 )
 
