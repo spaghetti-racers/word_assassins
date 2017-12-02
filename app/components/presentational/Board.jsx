d05f7af940460f7cf83c8cde09fb2eb840ce2604
Carmen <cperalta26@gmail.com>
2017-12-02 17:53:53 -0500

    fix: correctly render brown and white cards

--- app/components/presentational/Board.jsx
+++ app/components/presentational/Board.jsx
@@ -36,7 +36,7 @@ const Board = props => {
                   props.cards && props.cards.map((card, idx) =>
                     (
                       // LOGIC TO CHANGE COLOR AND WORD VIEW OF THE CARD UPON CLICK BASED ON COLOR/CLICKED ATTRIBUTES IN DB
-                      <Card onClick={card.clicked ? null : props.pickCard} style={{ backgroundColor: card.clicked ? card.color : 'tan' }} key={card.word}>
+                      <Card onClick={card.clicked ? null : props.pickCard} style={{ backgroundColor: card.clicked ? card.color : 'white' }} key={card.word}>
                         <Card.Content value={idx}>
                           {
                             card.clicked ? ' ' : <Card.Header value={idx}> {card.word} </Card.Header>
@@ -54,7 +54,7 @@ const Board = props => {
                   props.cards && props.cards.map((card, idx) =>
                     (
                       // LOGIC TO CHANGE COLOR AND WORD VIEW OF THE CARD UPON CLICK BASED ON COLOR/CLICKED ATTRIBUTES IN DB
-                      <Card style={{ backgroundColor: card.clicked ? card.color : 'tan' }} key={card.word}>
+                      <Card style={{ backgroundColor: card.clicked ? card.color : 'white' }} key={card.word}>
                         <Card.Content value={idx}>
                           {
                             card.clicked ? ' ' : <Card.Header value={idx}> {card.word} </Card.Header>
